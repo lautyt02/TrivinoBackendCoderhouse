5eb0f7f9c66513a5ccee8cf1ac577cd5c81aabfa
LautyT02 <lautyt02@gmail.com>
2023-12-01 05:14:04 -0300

    Ejercicio 2 terminado. No funciona correctamente

--- entrega-2/ProductManager.js
+++ entrega-2/ProductManager.js
@@ -10,7 +10,7 @@ class ProductManager {
     constructor() {
         this.path = './products.json'
         const createFile = async () => {
-            await fs.promises.writeFile(this.path)
+            await fs.promises.writeFile(this.path, '')
         }
         createFile()
         this.#id = 1
@@ -18,14 +18,23 @@ class ProductManager {
     }
     //Other methods
     //Extras
-    findIndex(id){
-        this.getProducts()
+    async returnIndex(id) {
+        await this.getProducts()
         return this.#products.findIndex(product => product.id === id)
     }
-    async #writeFile(){
-        await fs.promises.writeFile(this.path, this.#products.map(JSON.stringify))
+    async #writeFile() {
+        let arrayJSON = '[\n'
+        for (let i = 0; i < this.#products.length; i++) {
+            arrayJSON += '\t' + JSON.stringify(this.#products[i])
+            if (i != this.#products.length - 1) {
+                arrayJSON += ','
+            }
+            arrayJSON += '\n'
+        }
+        arrayJSON += ']'
+        await fs.promises.writeFile(this.path, arrayJSON)
     }
-    //Required by exercise
+    // //Required by exercise
     addProduct(title, description, price, thumbnail, code, stock) {
         this.getProducts()
         let required = typeof (title) == "string"
@@ -57,18 +66,17 @@ class ProductManager {
 
     }
     async getProducts() {
-        this.#products = await fs.promises.readFile(this.path).map(JSON.parse)
-        return this.#products
+        this.#products = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
     }
-    getProductById(id) {
-        this.getProducts()
+    async getProductById(id) {
+        await this.getProducts()
         return this.#products.find(product => product.id === id) ?? `No existe un producto con el id: ${id}`
     }
-    updateProduct(id, content) {
-        this.getProducts()
-        const findIndexResult = this.findIndex(id)
-        if (findIndexResult != -1) {
-            this.#products.splice(findIndexResult,1,{
+    async updateProduct(id, content) {
+        await this.getProducts()
+        const returnIndexResult = await this.returnIndex(id)
+        if (returnIndexResult != -1) {
+            this.#products.splice(returnIndexResult, 1, {
                 id: id,
                 title: content.title,
                 description: content.description,
@@ -77,14 +85,39 @@ class ProductManager {
                 code: content.code,
                 stock: content.stock
             })
-            this.#writeFile()
+            await this.#writeFile()
         }
     }
-    deleteProduct(id) {
-        const findIndexResult = this.findIndex(id)
-        if (findIndexResult != -1) {
-            this.#products.splice(findIndexResult,1)
-            this.#writeFile()
-        }   
+    async deleteProduct(id) {
+        const returnIndexResult = await this.returnIndex(id)
+        if (returnIndexResult != -1) {
+            this.#products.splice(returnIndexResult, 1)
+            await this.#writeFile()
+        }
     }
 }
+
+//Execution
+//Creacion de objeto perteneciente a ProductManager
+const myProductManager = new ProductManager()
+const tareasAsincronas = async () => {
+    // Agregado de varios Productos
+    await myProductManager.addProduct("Tux", "mascota del SO Linux", 5000, '/images/tux.png', 1500, 9999)
+    await myProductManager.addProduct("Pop_OS", "Distro Linux Desarrollada por System76", 10000, '/images/popos.png', 1600, 1000)
+    await myProductManager.addProduct("Linux Mint", "Distro Linux de desarrollo comunitario", 10000, '/images/popos.png', 1700, 1500)
+    await myProductManager.addProduct("The Unlicense", "Licencia para ceder tu código/contribuciones al Dominio Público", 10000, '/images/popos.png', 2000, 1)
+    //Intentar agregar un elemento sin todos los parametros
+    await myProductManager.addProduct('', "mascota del SO Linux", 0, '/images/tux.png', 1550)
+    //Intentar agregar un elemento con un code existente
+    await myProductManager.addProduct("Tux 3", "mascota del SO Linux", 5000, '/images/tux.png', 1500, 9999)
+    //Buscar producto por id
+    console.log('buscar por id')
+    console.log(await myProductManager.getProductById(3))
+    //Buscar producto por id inexistente
+    console.log(await myProductManager.getProductById(1000))
+    //Eliminar Producto
+    await myProductManager.deleteProduct(2)
+    //Actualizar Producto
+    await myProductManager.updateProduct(1,{title:"Tux modificado",description:"mascota del SO Linux",price:5000,thumbnail:"/images/tux.png",code:1500,stock:9999},)
+}
+tareasAsincronas()
