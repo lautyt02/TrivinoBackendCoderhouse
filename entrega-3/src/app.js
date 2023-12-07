2af73cb82f72fd03d3e8bdb91bf8bc481c9d75e6
LautyT02 <lautyt02@gmail.com>
2023-12-07 19:15:28 -0300

    Added express to app.js

--- entrega-3/src/app.js
+++ entrega-3/src/app.js
@@ -1,3 +1,9 @@
+//Imports
 import { ProductManager } from "./ProductManager";
-
+import express from 'express'
+//Config
 const myProductManager = new ProductManager()
+const app = express()
+app.use(express.urlencoded({extended: true}))
+//Run Server
+app.listen(8080,()=>console.log('server runing'))
