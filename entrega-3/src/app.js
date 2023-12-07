7410d5a79270a241417cf73b005163272e1be8fd
LautyT02 <lautyt02@gmail.com>
2023-12-07 20:52:37 -0300

    Finished entrega-3

--- entrega-3/src/app.js
+++ entrega-3/src/app.js
@@ -1,26 +1,32 @@
 //Imports
-import { ProductManager } from "./ProductManager";
+import { ProductManager } from "./ProductManager.js"
 import express from 'express'
 //Config
 const myProductManager = new ProductManager()
 const app = express()
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 //Requests
-app.get('products',async (req,res)=>{
-    const limit = req.query.limit
+app.get('/products', async (req, res) => {
+    let limit = req.query.limit
     const myProducts = await myProductManager.getProducts()
-    if(!limit){
-        let myLimitedProducts =[]
-        for(let i=0; i<limit;i++){
+    if(limit>myProducts.length){
+        limit=myProducts.length
+    }
+    if (limit) {
+        let myLimitedProducts = []
+        for (let i = 0; i < limit; i++) {
             myLimitedProducts.push(myProducts[i])
         }
         res.send(myLimitedProducts)
     }
-    res.send(myProducts)
+    else {
+        res.send(myProducts)
+    }
+
 })
-app.get('products/:pid',async (req,res)=>{
-    const id=req.params.pid
+app.get('/products/:pid', async (req, res) => {
+    const id = parseInt(req.params.pid)
     res.send(await myProductManager.getProductById(id))
 })
 //Run Server
-app.listen(8080,()=>console.log('server runing'))
+app.listen(8080, () => console.log('server runing'))
