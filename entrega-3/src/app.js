e51e5af6175faa7b1e01527d0d38837e3498dea0
LautyT02 <lautyt02@gmail.com>
2023-12-07 19:34:02 -0300

    Wrote request for all products with limit query

--- entrega-3/src/app.js
+++ entrega-3/src/app.js
@@ -5,5 +5,18 @@ import express from 'express'
 const myProductManager = new ProductManager()
 const app = express()
 app.use(express.urlencoded({extended: true}))
+//Requests
+app.get('products',async (req,res)=>{
+    const {limit} = req.query
+    const myProducts = await myProductManager.getProducts()
+    if(!limit){
+        let myLimitedProducts =[]
+        for(let i=0; i<limit;i++){
+            myLimitedProducts.push(myProducts[i])
+        }
+        res.send(myLimitedProducts)
+    }
+    res.send(myProducts)
+})
 //Run Server
 app.listen(8080,()=>console.log('server runing'))
