386e22a1e69902040595b6db1ee46da126d009a6
LautyT02 <lautyt02@gmail.com>
2024-01-21 21:38:34 -0300

    Modified Products Router
    Imported socket server and added io.emit to update clients

--- entrega-5/src/routes/products.router.js
+++ entrega-5/src/routes/products.router.js
@@ -1,9 +1,13 @@
 //Imports
 import { Router } from "express"
-import { ProductManager } from "../ProductManager.js"
+import { ProductManager } from "../classes/ProductManager.js"
+
+import { io } from "../app.js"
+
 //Declaraciones
 const productsRouter = Router()
 const myProductManager = new ProductManager()
+
 //GETs
 productsRouter.get('/', async (req, res) => {
     let limit = req.query.limit
@@ -32,6 +36,9 @@ productsRouter.post('/', async (req, res) => {
     const {title, description, code, price, stock, category, thumbnails} = await req.body
     const message = await myProductManager.addProduct(title, description, code, price, stock, category, thumbnails)
     if (message == "Producto añadido con éxito") {
+        const myProducts = await myProductManager.getProducts()
+        io.emit('updateClients', myProducts)
+        console.log('sended updateServer Event')
         res.status(201).json({ status: "success", message: message })
     }
     else {
@@ -45,6 +52,8 @@ productsRouter.put('/:pid',async (req, res) => {
     const newData= await req.body
     const message = await myProductManager.updateProduct(id,newData)
     if(message == "Producto modificado con éxito"){
+        const myProducts = await myProductManager.getProducts()
+        io.emit('updateClients', myProducts)
         res.json({ status: "success", message: message })
     }
     else{
@@ -56,6 +65,8 @@ productsRouter.delete('/:pid',async (req, res) => {
     const id = parseInt(req.params.pid)
     const message = await myProductManager.deleteProduct(id)
     if(message == "Producto eliminado con éxito"){
+        const myProducts = await myProductManager.getProducts()
+        io.emit('updateClients', myProducts)
         res.json({ status: "success", message: message })
     }
     else{
