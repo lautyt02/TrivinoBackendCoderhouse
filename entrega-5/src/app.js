8b79abd3b3031e14d09d062a1c333bacd2a11b02
LautyT02 <lautyt02@gmail.com>
2024-01-21 21:50:41 -0300

    Added handlebars and websockets

--- entrega-5/src/app.js
+++ entrega-5/src/app.js
@@ -1,14 +1,33 @@
 //Imports
 import express from 'express'
+import handlebars from 'express-handlebars'
+import {Server} from 'socket.io'
 import { productsRouter } from "./routes/products.router.js"
 import { cartsRouter } from "./routes/carts.router.js"
+import { viewsRouter } from './routes/views.router.js'
+import {port} from './configs/server.config.js'
 //Config
-const port=8080
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('../public'))
-const server = app.listen(port, () => console.log('server runing'))//Run Server
+//Handlebars Config
+app.engine('handlebars', handlebars.engine())
+app.set('views', process.cwd() + '/views')
+app.set('view engine', 'handlebars')
+
+//Run Server
+const httpServer = app.listen(port, () => console.log(`server runing at port ${port}`))
+const io = new Server(httpServer)
+io.on('connection', socket =>{
+    console.log(socket.id)
+})
+// io.sockets.on('updateServer', updatedProducts =>{
+//     console.log("Server updated")
+//     io.emit('updateClients',updatedProducts)
+// })
 //Requests
 app.use('/api/products/', productsRouter)
 app.use('/api/carts/', cartsRouter)
+app.use('/', viewsRouter)
+export {io}
